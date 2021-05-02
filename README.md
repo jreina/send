9cf9623487714f8442231c0ce5ac3c5789513895
Johnny Reina <johnny.r.reina@gmail.com>
2021-05-01 23:35:22 -0500

    :rocket:

---
+++ README.md
@@ -0,0 +1,17 @@
+# `send`
+
+Share files over LAN via QR code.
+
+## Install
+
+`npm i -g @johnny.reina/send`
+
+## Usage
+
+Run the following:
+
+```bash
+send memes.png
+```
+
+This will open a browser with a QR code. Scan the QR code with your phone or share the URL below the code to someone on your LAN. You can use the download page to download the file being shared.
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const os = require("os");
+
+const open = require("open");
+const express = require("express");
+const { program } = require("commander");
+const qrcode = require("qrcode");
+const { v4: uuid } = require("uuid");
+const app = express();
+
+program
+  .name("send")
+  .arguments("<file>")
+  .action(async function (file) {
+    const fullPath = path.resolve(file);
+    const filename = path.basename(fullPath);
+    const serverPath = `http://${os.hostname()}:4000/`;
+    const downloadPage = `${serverPath}download`;
+    const guid = uuid();
+    const fileUrl = `${serverPath}${guid}`;
+    const qr = await qrcode.toDataURL(downloadPage);
+
+    // // spin up server
+    app.get("/", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "static/index.html"));
+    });
+
+    app.get("/code.png", (req, res) => {
+      res.json({ dataUrl: qr, url: downloadPage });
+    });
+
+    app.get(`/metadata.json`, (req, res) => {
+      res.json({
+        filename,
+        url: fileUrl,
+      });
+    });
+
+    app.get(`/${guid}`, (req, res) => {
+      res.download(fullPath);
+    });
+
+    app.get("/download", (req, res) => {
+      console.log(fullPath);
+      res.sendFile(path.resolve(__dirname, "static/download.html"));
+    });
+    app.listen(4000);
+    open(serverPath);
+  });
+
+program.parse(process.argv);
